67d08b4e4fb945bfed017f103a64089479662eec
小柒道 <60565076+xiaoqidaov2@users.noreply.github.com>
2024-11-12 09:47:51 +0800

    Delete game directory

--- game/__init__.py
+++
@@ -1,7 +0,0 @@
-from .game import Game
-from .player import Player
-from .fishing_system import FishingSystem
-from .shop import Shop
-from .item import Item
-from .equipment import Equipment
-__all__ = ['Game', 'Player', 'FishingSystem', 'Shop', 'Item', 'Equipment']
