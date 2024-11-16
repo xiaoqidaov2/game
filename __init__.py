381f6b3fb4794f9ee6ed6bbab86720161b3e37ef
小柒道 <60565076+xiaoqidaov2@users.noreply.github.com>
2024-11-16 11:33:01 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,4 +4,5 @@ from .fishing_system import FishingSystem
 from .shop import Shop
 from .item import Item
 from .equipment import Equipment
-__all__ = ['Game', 'Player', 'FishingSystem', 'Shop', 'Item', 'Equipment']
+from .monopoly import MonopolySystem
+__all__ = ['Game', 'Player', 'FishingSystem', 'Shop', 'Item', 'Equipment', 'MonopolySystem']
